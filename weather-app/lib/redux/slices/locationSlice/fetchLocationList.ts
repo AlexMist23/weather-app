5196f85cde2bd229310dce53c21030cf2d5db103
Misterkiewicz Aleksander <aleksander.misterkiewicz@outlook.com>
2023-11-09 17:38:42 +0100

    s

--- weather-app/lib/redux/slices/locationSlice/fetchLocationList.ts
+++ weather-app/lib/redux/slices/locationSlice/fetchLocationList.ts
@@ -2,15 +2,17 @@ import { APIKey } from "./APIKey";
 
 export const fetchLocationList = async (
   city: string = ""
-): Promise<Array<{
-  name: String;
-  country: String;
-  state: String;
-  lat: number;
-  lon: number;
-}>> => {
+): Promise<
+  Array<{
+    name: String;
+    country: String;
+    state: String;
+    lat: number;
+    lon: number;
+  }>
+> => {
   const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`
   );
   let result = await response.json();
   return result;
