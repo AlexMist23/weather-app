ad15471b60840599e7210c3841e3bb5bbd8db3c6
Misterkiewicz Aleksander <aleksander.misterkiewicz@outlook.com>
2023-11-13 16:49:39 +0100

    API usage corrections

--- weather-app/lib/tempConvert.tsx
+++ weather-app/lib/tempConvert.tsx
@@ -1,5 +1,4 @@
 export const convertTemp = (temp: number | undefined, scale: string) => {
-    console.log(scale)
   if (typeof temp === "undefined") return;
   if (scale == "Celcius") return `${(temp - 273.15).toFixed(0)}°C`;
   else return `${(temp * 1.8 - 459.67).toFixed(0)}°F`;
