cfdf5bdaf54ee0a7708d0a3b1cb1fc43251045c0
Misterkiewicz Aleksander <aleksander.misterkiewicz@outlook.com>
2023-11-21 17:11:12 +0100

    location css

--- app/components/LocationPanel/LocationName/LocationName.tsx
+++ app/components/LocationPanel/LocationName/LocationName.tsx
@@ -7,10 +7,9 @@ import styles from "./locationname.module.css";
 
 export const Location: React.FC<LocationProps> = ({ name, country, state }) => {
   return (
-    <div>
-      <h1 className={styles.locationName}>{name}</h1>
-      <h2 className={styles.locationCountry}>{country}</h2>
-      <h2 className={styles.locationCountry}>{state}</h2>
+    <div className={styles.div}>
+      <h1 className={styles.h1}>{name}</h1>
+      <h2 className={styles.h2}>{country && `${country}, `}{state}</h2>
     </div>
   );
 };
