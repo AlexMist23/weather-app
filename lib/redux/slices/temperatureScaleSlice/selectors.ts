068b5f3c1958d722b86725a11c6a950d7ff5fd30
Alex <aleksander.misterkiewicz@outlook.com>
2023-11-24 14:08:18 +0100

    add temp symbol selector

--- lib/redux/slices/temperatureScaleSlice/selectors.ts
+++ lib/redux/slices/temperatureScaleSlice/selectors.ts
@@ -3,3 +3,6 @@ import type { ReduxState } from "@/lib/redux";
 
 export const selectTemperatureScale = (state: ReduxState) =>
   state.temperatureScale.scale;
+
+  export const selectTemperatureSymbol = (state: ReduxState) =>
+  state.temperatureScale.symbol;
