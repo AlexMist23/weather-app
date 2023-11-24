2d3bc06207b3249a64fd3f6a18c0dde23433e786
Alex <aleksander.misterkiewicz@outlook.com>
2023-11-24 12:52:56 +0100

    1

--- lib/redux/slices/locationListSlice/fetchLocationList.ts
+++ lib/redux/slices/locationListSlice/fetchLocationList.ts
@@ -2,7 +2,7 @@ import { type location } from "../..";
 
 export const fetchLocationList = async (
   city: string
-): Promise<Array<location>> => {
+): Promise<location> => {
   const response = await fetch("/api/location-list", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
