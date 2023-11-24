68d1f7114836c103d97022e3ccb1e2a3ffe7badf
Alex <aleksander.misterkiewicz@outlook.com>
2023-11-24 12:54:28 +0100

    fetch data type change

--- lib/redux/slices/locationListSlice/fetchLocationList.ts
+++ lib/redux/slices/locationListSlice/fetchLocationList.ts
@@ -2,7 +2,7 @@ import { type location } from "../..";
 
 export const fetchLocationList = async (
   city: string
-): Promise<location> => {
+): Promise<{ data: Array<location> }> => {
   const response = await fetch("/api/location-list", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
