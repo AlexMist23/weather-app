c279fc9cbc06f45db91e46fa9313b830ff45ac47
Alex <aleksander.misterkiewicz@outlook.com>
2023-11-24 17:44:46 +0100

    local date correct use

--- app/components/LocationPanel/LocalDate/LocalDate.tsx
+++ app/components/LocationPanel/LocalDate/LocalDate.tsx
@@ -9,12 +9,21 @@ export const LocalDate: React.FC<{ weather: currentWeather }> = ({
   weather,
 }) => {
   const currentWeather = weather;
-
   const date = currentWeather && new Date(+`${currentWeather?.dt}000`);
+
+  function getLang() {
+    if (!navigator.languages) return navigator.languages[0];
+    return navigator.language;
+  }
 
   return (
     <p className={styles.p}>
-      {date?.toDateString()} {date?.toLocaleTimeString()}
+      {`${date.toLocaleString(getLang(), {
+        month: "short",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+      })}`}
     </p>
   );
 };
