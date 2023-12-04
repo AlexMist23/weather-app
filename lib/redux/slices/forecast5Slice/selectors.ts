7c089aca1553859b70e862c95b590a5aa3fce267
Alex <aleksander.misterkiewicz@outlook.com>
2023-12-04 16:41:38 +0100

    add types in selector

--- lib/redux/slices/forecast5Slice/selectors.ts
+++ lib/redux/slices/forecast5Slice/selectors.ts
@@ -1,16 +1,23 @@
 /* Instruments */
-import type { ReduxState } from "@/lib/redux";
+import type { ReduxState, Forecast5ListElement } from "@/lib/redux";
 
 export const selectForecast5 = (state: ReduxState) => state.forecast5;
 
-export const selectForecast5DateList = (state: ReduxState) => state.forecast5.data?.list.reduce((acc: Record<string, Record<string, T[]>>, obj: T) => {
-    const dt = new Date(obj.dt * 1000);
-    const dateKey = dt.toLocaleDateString();
-    const dayKey = dt.getDate().toString();
+export const selectForecast5DateList = (state: ReduxState) =>
+  state.forecast5.data?.list.reduce(
+    (
+      acc: Record<string, Record<string, Forecast5ListElement[]>>,
+      obj: Forecast5ListElement
+    ) => {
+      const dt = new Date(obj.dt * 1000);
+      const dateKey = dt.toLocaleDateString();
+      const dayKey = dt.getDate().toString();
 
-    acc[dateKey] = acc[dateKey] || {};
-    acc[dateKey][dayKey] = acc[dateKey][dayKey] || [];
-    acc[dateKey][dayKey].push(obj);
+      acc[dateKey] = acc[dateKey] || {};
+      acc[dateKey][dayKey] = acc[dateKey][dayKey] || [];
+      acc[dateKey][dayKey].push(obj);
 
-    return acc;
-  }, {});;
+      return acc;
+    },
+    {}
+  );
