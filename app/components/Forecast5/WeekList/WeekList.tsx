9f69ed8a072a03811227d701fe4b3d6f04160e58
Alex <aleksander.misterkiewicz@outlook.com>
2023-12-05 11:11:37 +0100

    update WeekList.tsx

--- app/components/Forecast5/WeekList/WeekList.tsx
+++ app/components/Forecast5/WeekList/WeekList.tsx
@@ -4,39 +4,56 @@
 import { useState, useEffect } from "react";
 
 /* Instruments */
-import type {Forecast5ListElement } from "@/lib/redux";
+import {
+  useSelector,
+  type Forecast5ListElement,
+  selectActiveDate,
+  useDispatch,
+  activeDateSlice,
+} from "@/lib/redux";
 
 /* CSS */
 import styles from "./weeklist.module.css";
+import {format} from "date-fns";
 
 export const WeekList: React.FC<WeekListProps> = ({ sortedList }) => {
-  const [activeWeekDay, setActiveWeekDay] = useState<string>('');
+  const dispatch = useDispatch();
+  const activeDate = new Date(useSelector(selectActiveDate))
+
   const weekListSorted = sortedList;
 
-  useEffect(() => {
-    const firstDateKey = Object.keys(weekListSorted)[0];
-    if (firstDateKey) {
-      setActiveWeekDay(firstDateKey);
+  const liClickHandle = (monthYearKey:string) => {
+    const date = new Date(monthYearKey)
+    if (activeDate.getFullYear() != date.getFullYear()){
+      dispatch(activeDateSlice.actions.setYear(date.getFullYear()))
     }
-  }, []);
+    if (activeDate.getMonth() != date.getMonth()){
+      dispatch(activeDateSlice.actions.setMonth(date.getMonth()))
+    }
+    if (activeDate.getDate() != date.getDate()){
+      dispatch(activeDateSlice.actions.setDay(date.getDate()))
+    }
+  }
 
   return (
     <ul className={styles.ul}>
       {Object.keys(weekListSorted).map((monthYearKey, index) => (
         <li
-          onClick={() => setActiveWeekDay(monthYearKey)}
-          className={`${styles.li} ${monthYearKey === activeWeekDay ? styles.liActive : ''}`}
+          onClick={() => liClickHandle(monthYearKey)}
+          className={`${styles.li} ${
+            monthYearKey === format(activeDate, 'yyyy-MM-dd') ? styles.liActive : ""
+          }`}
           key={index}
         >
           <p>
-            {new Date(monthYearKey).toLocaleString('en-us', {
-              weekday: 'short',
+            {new Date(monthYearKey).toLocaleString("en-us", {
+              weekday: "short",
             })}
           </p>
           <p>
-            {new Date(monthYearKey).toLocaleString('en-us', {
-              day: '2-digit',
-              month: '2-digit',
+            {new Date(monthYearKey).toLocaleString("en-us", {
+              day: "2-digit",
+              month: "2-digit",
             })}
           </p>
         </li>
